67b95db8d7033b1eac5d4259ac6447009b11217c
kilianspeder <kilianspeder@icloud.com>
2024-01-31 20:22:36 +0100

    basic communication works

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 import { GrandMA2 } from "grandma2";
 import { XTouchCompactMidiController } from "midiabstract";
-
-var ma2 = new GrandMA2("10.1.1.1", "remote", "remote", [])
+const ma2 = new GrandMA2("10.1.1.1", "remote", "remote", ["fader","button"], 8, 1, 5, 101)
 var xt = new XTouchCompactMidiController()
 
 // console.log(xt.textButtons["buttonA"].button.event_emitter)
-xt.textButtons["buttonA"].button.event_emitter.on("press", () => {ma2.decreasePage()});
-xt.textButtons["buttonB"].button.event_emitter.on("press", () => {ma2.increasePage()});
+xt.textButtons["buttonA"].button.event_emitter.on("press", () => {ma2.decreasePage('faderPage')});
+xt.textButtons["buttonB"].button.event_emitter.on("press", () => {ma2.increasePage('faderPage')});
 
 xt.faders.forEach((fader, index) => {
-    fader.fader.event_emitter.on("value", (value) => {ma2.setExecFaderValue(index + 1, value)})
+    fader.fader.event_emitter.on("value", (value) => {
+        console.log(value)
+        ma2.setExecFaderValue(index + 1, value)
+        })
 })
 
-ma2.on("executerFaderChanged", (e) => {
-    e.map((item) => {
-        let id = item.executerNumber;
-        xt.faders[id - 1].motor.set(item.executerBlock.fader.value)
-        xt.buttons[1][id - 1].lamp.set(item.executerBlock.button3.state)
-        xt.buttons[2][id - 1].lamp.set(item.executerBlock.button2.state)
-        xt.buttons[3][id - 1].lamp.set(item.executerBlock.button1.state)
+
+ma2.on("executorFaderChanged", (e) => {
+    console.log("change")
+    //console.log(JSON.stringify(e))
+    e[0].map((item) => {
+        console.log(JSON.stringify(item))
+        let id = item.executorNumber;
+        xt.faders[id - 1].motor.set(item.executorBlocks[0].fader.value)
+        xt.buttons[1][id - 1].lamp.set(item.executorBlocks[0].button3.state)
+        xt.buttons[2][id - 1].lamp.set(item.executorBlocks[0].button2.state)
+        xt.buttons[3][id - 1].lamp.set(item.executorBlocks[0].button1.state)
     })
 })
+
